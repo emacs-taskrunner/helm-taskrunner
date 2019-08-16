a1729b8cc210a94e30fd929882aacf07d869bafc
Yavor Konstantinov <ykonstantinov1@gmail.com>
2019-08-15 23:40:43 -0700

    Add function to check if current buffer is in a project

--- helm-taskrunner.el
+++ helm-taskrunner.el
@@ -9,37 +9,36 @@
 (defvaralias 'helm-taskrunner-gradle-heading-regexps 'taskrunner-gradle-heading-regexps)
 (defvaralias 'helm-taskrunner-ant-tasks-buffer-name 'taskrunner-ant-tasks-buffer-name)
 
-(defun helm-taskrunner ()
-  "Launch helm to select a task which is ran in the currently visited project."
-  (interactive)
+(defun helm-taskrunner--check-if-in-project ()
+  "Check if the currently visited buffer is in a project.
+If it is not then prompt the user to select a project."
   (let ((in-project-p (projectile-project-p)))
     (when (not in-project-p)
       (if (package-installed-p 'helm-projectile)
           (helm-projectile-switch-project)
         (projectile-switch-project))
       )
+    )
+  )
 
-    (when (projectile-project-p)
-      (helm :sources (helm-build-sync-source "helm-taskrunner-tasks"
-                       :candidates (taskrunner-get-tasks-from-cache))
-            :prompt "Task to run: "
-            :buffer "*helm taskrunner*")
-      )
+(defun helm-taskrunner ()
+  "Launch helm to select a task which is ran in the currently visited project."
+  (interactive)
+  (helm-taskrunner--check-if-in-project)
+
+  (when (projectile-project-p)
+    (helm :sources (helm-build-sync-source "helm-taskrunner-tasks"
+                     :candidates (taskrunner-get-tasks-from-cache))
+          :prompt "Task to run: "
+          :buffer "*helm taskrunner*")
     )
   )
 
 (defun helm-taskrunner-rerun-last-command ()
   "Rerun the last task ran in the currently visited project."
   (interactive)
-  (let ((in-project-p (projectile-project-p)))
-    (when (not in-project-p)
-      (if (package-installed-p 'helm-projectile)
-          (helm-projectile-switch-project)
-        (projectile-switch-project))
-      )
-
-    (when (projectile-project-p)
-      (taskrunner-rerun-last-task (projectile-project-root))
-      )
+  (helm-taskrunner--check-if-in-project)
+  (when (projectile-project-p)
+    (taskrunner-rerun-last-task (projectile-project-root))
     )
   )
